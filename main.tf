60db465d8bd7e7286671f370698b32638377a971
Leif Eriksen <leif.eriksen.au@gmail.com>
2021-02-15 20:59:40 +1100

    test vcs integration

--- main.tf
+++ main.tf
@@ -58,7 +58,7 @@ resource "azurerm_resource_group" "rg" {
 
 resource "azurerm_key_vault" "akv" {
   location            = module.subscription.location
-  name                = "v-lab3-${var.TERRAFORM_WORKSPACE}"
+  name                = "vcs-lab3-${var.TERRAFORM_WORKSPACE}"
   resource_group_name = azurerm_resource_group.rg.name
   sku_name            = module.environment.key_vault_sku
   tenant_id           = module.globals.tenant_id
