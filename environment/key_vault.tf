da89f15b1675a55d40cbe22a02c7c6753dd6b858
Leif Eriksen <a-leriksen@ausnetservices.com.au>
2021-04-21 20:06:04 +1000

    alignment

--- environment/key_vault.tf
+++ environment/key_vault.tf
@@ -2,7 +2,7 @@ module "key_vault" {
   source              = "../modules/key_vault"
   location            = module.subscription.location
   name                = "vcs-lab3-${var.TF_WORKSPACE}"
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = module.resource_group.name
   sku_name            = module.environment.key_vault_sku
   tenant_id           = module.globals.tenant_id
   tags                = local.tags
